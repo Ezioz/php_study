fac676bac3fdeb569c328368f2bb870acc4b7286
ahtoh <472668561@qq.com>
2020-12-21 13:36:22 +0800

    九九乘法表

---
+++ multiplication_table/multiplication_table.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>九九乘法表</title>
+</head>
+<body>
+
+<?php 
+
+for ($i=1; $i < 10; $i++) {
+	//$j控制的是第一个乘数，$i控制的是第二个乘数
+	for ($j=1; $j <= $i; $j++) { 
+		//\n，\t等内容要使用双引号
+		echo "$j * $i = ". $i * $j . "\t";
+	}
+	echo "<br/>";
+}
+ ?>
+</body>
+</html>
